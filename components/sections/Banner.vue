7187f608046ee095e79a58babf364cd7f7879496
oukhrib <abderrahimoukhrib@gmail.com>
2021-06-26 14:54:44 +0100

    Dynamic banner and collections

--- components/sections/Banner.vue
+++ components/sections/Banner.vue
@@ -1,15 +1,15 @@
 <template>
     <div class="container">
-        <div class="flex p-2">
-            <div class="md:w-3/4 relative overflow-hidden">
-                <div class="flex h-full mr-2 slider-item" :class="'last'" v-if="lastItem">
-                    <div class="image md:w-2/3 relative">
-                        <img class="h-full object-cover" :src="lastItem.image" :alt="lastItem.title">
+        <div class="flex flex-wrap p-2">
+            <div class="w-full md:w-3/4 relative overflow-hidden">
+                <div class="flex flex-wrap h-full mr-2 slider-item" :class="'last'" v-if="lastItem">
+                    <div class="image w-full md:w-2/3 relative">
+                        <si-image width="600" height="300" class="h-48 bg-green-200 md:h-48 w-full object-cover" :src="lastItem.image ? lastItem.image.src: null" :alt="lastItem.title"/>
                         <div class="flex absolute bottom-0 p-2">
                             <span v-for="(x,y) in banner.items" :key="y" :class="y==activeIndex ?'bg-green-200': ''" class="w-4 h-4 rounded-full bg-white mr-1 flex cursor-pointer" @click="activeIndex=y"></span>
                         </div>
                     </div>
-                    <div class="text md:w-1/3 bg-primary text-white">
+                    <div class="text w-full md:w-1/3 bg-primary text-white">
                         <div class="p-2 flex flex-col justify-between h-full">
                             <h2 class="text-2xl mb-2">{{ lastItem.title }}</h2>
                             <p class="mb-2">{{ lastItem.description }}</p>
@@ -18,14 +18,14 @@
                     </div>
                 </div>
                 <template v-for="(item,i) in banner.items">
-                    <div class="flex h-full mr-2 slider-item" :class="i==activeIndex ? 'active': ''" :key="i">
-                        <div class="image md:w-2/3 relative">
-                            <img class="h-full object-cover" :src="item.image" :alt="item.title">
+                    <div class="flex flex-wrap h-full mr-2 slider-item" :class="i==activeIndex ? 'active': ''" :key="i">
+                        <div class="image w-full md:w-2/3 relative">
+                            <si-image width="100%" height="300" class="h-48 bg-green-200 md:h-full object-cover" :src="item.image ? item.image.src : null" :alt="item.title"/>
                             <div class="flex absolute bottom-0 p-2">
                                 <span v-for="(x,y) in banner.items" :key="y" :class="y==activeIndex ?'bg-green-200': ''" class="w-4 h-4 rounded-full bg-white mr-1 flex cursor-pointer" @click="animate(y)"></span>
                             </div>
                         </div>
-                        <div class="text md:w-1/3 bg-primary text-white">
+                        <div class="text w-full md:w-1/3 bg-primary text-white">
                             <div class="p-2 flex flex-col justify-between h-full">
                                 <h2 class="text-2xl mb-2">{{ item.title }}</h2>
                                 <p class="mb-2">{{ item.description }}</p>
@@ -35,10 +35,10 @@
                     </div>
                 </template>
             </div>
-            <div class="md:w-1/4 bg-yellow-300">
+            <div class="w-full hidden md:block md:w-1/4 bg-yellow-300">
                 <div class="h-full">
                     <router-link :to="banner.right.url">
-                        <img :src="banner.right.image" alt="Banner image" srcset="">
+                        <si-image class=" object-cover h-full" width="300" height="500" :src="banner.right.image ? banner.right.image.src: null" alt="Banner image"/>
                     </router-link>
                 </div>
             </div>
@@ -52,34 +52,7 @@ export default {
             timeout: null,
             lastItem: null,
             activeIndex: 0,
-            banner: {
-                items: [
-                    { 
-                        image: "https://www.free-wallpapers.su/data/media/2278/fr0282.jpg",
-                        title: "Renal & Urology Nutrition News, Diet Tips",
-                        description: "Greater consumption of healthful plant-based foods is associated with significantly reduced risks for advanced, lethal, and fatal prostate cancer, especially among men younger than 65 years",
-                        button: {
-                            active: true,
-                            url: '/shop',
-                            text: "Order now"
-                        }
-                    },
-                    { 
-                        image: "https://www.burgerking.it/site/assets/files/1926/crispy-chicken.jpg",
-                        title: "Renal & Urology Nutrition News, Diet Tips",
-                        description: "Greater consumption of healthful plant-based foods is associated with significantly reduced risks for advanced, lethal, and fatal prostate cancer, especially among men younger than 65 years",
-                        button: {
-                            active: true,
-                            url: '/shop',
-                            text: "Order now"
-                        }
-                    }
-                ],
-                right: {
-                    image: "https://webtoolfeed.files.wordpress.com/2012/04/food-01.jpg",
-                    url: '/shop',
-                }
-            }
+            banner: this.$store.state.settings.sections.banner
         }
     },
     mounted() {
--- components/sections/Collections.vue
+++ components/sections/Collections.vue
@@ -14,73 +14,17 @@
 export default {
   data() {
     return {
-      items: [
-        {
-          image: {
-            title: "image",
-            _id: "611d7965af093e19cef7dacc",
-            src: "https://storeno.b-cdn.net/stores/8-2021/1629321573307.jpeg",
-          },
-          parent: null,
-          childrens: [],
-          _id: "611d737caf093e19cef7d757",
-          name: "Bluetooth",
-          slug: "bluetooth",
-          description: "",
-          translates: [],
-          storeId: "611d702e23de571fb9b97d94",
-          __v: 0,
-        },
-        {
-          /* image: {
-            title: "image",
-            _id: "611d7a4eaf093e19cef7dbce",
-            src: "https://storeno.b-cdn.net/stores/8-2021/1629321806390.jpeg",
-          }, */
-          parent: null,
-          childrens: [],
-          _id: "611d73f5af093e19cef7d7a9",
-          name: "Computers",
-          slug: "computers",
-          description: "",
-          translates: [],
-          storeId: "611d702e23de571fb9b97d94",
-          __v: 0,
-        },
-        {
-          image: {
-            title: "image",
-            _id: "611d7a03af093e19cef7dbbf",
-            src: "https://storeno.b-cdn.net/stores/8-2021/1629321731162.webp",
-          },
-          parent: null,
-          childrens: [],
-          _id: "611d73b4af093e19cef7d77f",
-          name: "Glasses",
-          slug: "glasses",
-          description: "",
-          translates: [],
-          storeId: "611d702e23de571fb9b97d94",
-          __v: 0,
-        },
-        {
-          image: {
-            title: "image",
-            _id: "611d78a5af093e19cef7d9f6",
-            src: "https://storeno.b-cdn.net/stores/8-2021/1629321381575.webp",
-          },
-          parent: null,
-          childrens: [],
-          _id: "611d7334af093e19cef7d73f",
-          name: "headphones",
-          slug: "headphones",
-          description: "",
-          translates: [],
-          storeId: "611d702e23de571fb9b97d94",
-          __v: 0,
-        },
-      ],
+      items: [],
     };
   },
+  async fetch(){
+      try{
+          const { data } = await this.$storeino.collections.search()
+          this.items = data.results
+          this.loading = false;
+      }catch(e){
+        console.log({e});
+      }
+  }
 };
 </script>
