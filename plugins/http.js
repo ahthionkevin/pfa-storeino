8f43df0966b8400cfef627ecdba701025e80ceef
oukhrib <abderrahimoukhrib@gmail.com>
2021-10-21 13:02:32 +0100

    DEV :: Fix host

--- plugins/http.js
+++ plugins/http.js
@@ -1,10 +1,10 @@
 import https from 'https';
 let baseURL = 'https://api-stores.storeino.world/api';
-if (process.env.environment !== 'production') baseURL = "https://api-stores.storeino.world/api";
+if (process.env.environment == 'production') baseURL = "https://api-stores.storeino.com/api";
 export default function ({ $axios, store, app }, inject) {
   if(process.server) {
     if (app.context.req && app.context.req.headers && app.context.req.headers['x-auth-token']) {
-      baseURL = 'http://api-stores:3031/api';
+      // baseURL = 'http://api-stores:3031/api';
       store.state.token = app.context.req.headers['x-auth-token'];
     }
   }
