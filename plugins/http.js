35e28389146ff513d43750e7ebf97d6981fea124
oukhrib <abderrahimoukhrib@gmail.com>
2021-10-26 16:24:37 +0100

    PROD :: Add production url

--- plugins/http.js
+++ plugins/http.js
@@ -1,6 +1,6 @@
 import https from 'https';
 let baseURL = 'https://api-stores.storeino.world/api';
-if (process.env.environment == 'production') baseURL = "https://api-stores.storeino.world/api";
+if (process.env.environment == 'production') baseURL = "https://api-stores.storeino.com/api";
 export default function ({ $axios, store, app }, inject) {
   if(process.server) {
     if (app.context.req && app.context.req.headers && app.context.req.headers['x-auth-token']) {
