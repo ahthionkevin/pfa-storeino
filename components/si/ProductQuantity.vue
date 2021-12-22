3a5b862e140be5a2324e062e23c68396d8e55bf0
oukhrib <abderrahimoukhrib@gmail.com>
2021-12-22 17:29:00 +0100

    Unit design

--- components/si/ProductQuantity.vue
+++ components/si/ProductQuantity.vue
@@ -6,7 +6,7 @@
                 <span class="px-2 py-4 px-5 text-center ">{{ value }}</span>
                 <input :min="quantity.min" :max="quantity.max" class="px-2 text-center absolute outline-none inset-0 bg-opacity-0 w-full h-full" v-model="value" type="number">
             </label>
-            <span v-if="quantity.unit">{{ quantity.unit }}</span>
+            <span class="flex items-center bg-white border px-2" v-if="quantity.unit">{{ quantity.unit }}</span>
         </div>
         <button @click="inc(1)" class="bg-gray-50 border py-2 px-4" :class="value < quantity.instock ? 'bg-primary text-white' : ''">+</button>
     </div>
