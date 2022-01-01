460cd515c5e867e626ea1b5bd89a71f3a52acfe2
oukhrib <abderrahimoukhrib@gmail.com>
2022-01-01 22:04:10 +0100

    PROD :: Fix collections url

--- components/si/Collection.vue
+++ components/si/Collection.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="w-full flex flex-col h-full bg-white shadow">
-        <nuxt-link class="flex pb-full relative" :to="url">
+        <nuxt-link class="flex pb-full relative" :to="item.childrens.length > 0 ? `/collections/${item.slug}` : `/shop/${item.slug}`">
             <si-image width="400" height="400" class="h-full w-full absolute inset-0  object-cover" :src="item.image ? item.image.src : null" :alt="item.name" srcset=""/>
         </nuxt-link>
-        <nuxt-link :to="url">
+        <nuxt-link :to="item.childrens.length > 0 ? `/collections/${item.slug}` : `/shop/${item.slug}`">
             <h3 class="p-2 text-center text-xl">{{ item.name }} <span v-if="item.childrens.length > 0">({{ item.childrens.length }})</span></h3>
         </nuxt-link>
     </div>
@@ -12,11 +12,6 @@
 export default {
     props: {
         item: Object
-    },
-    data() {
-        return {
-            url: this.item.childrens.length > 0 ? `/collections/${this.item.slug}` : `/shop/${this.item.slug}`
-        }
-    },
+    }
 }
 </script>
